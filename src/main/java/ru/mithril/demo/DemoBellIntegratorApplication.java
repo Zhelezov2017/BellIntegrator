5eb3988599705ee35ed578677dd7c423ac40cec1
Vlad <Torino111@mail.ru>
2019-04-07 17:52:51 +0300

    One step

---
+++ src/main/java/ru/mithril/demo/DemoBellIntegratorApplication.java
@@ -0,0 +1,13 @@
+package ru.mithril.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBellIntegratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBellIntegratorApplication.class, args);
+    }
+
+}
