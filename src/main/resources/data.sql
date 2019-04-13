089c3804eaa0fd2b069aff40f4a2ec5af32abd4e
Vlad <Torino111@mail.ru>
2019-04-14 00:52:11 +0300

    Finish schema.sql and data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,8 @@
-INSERT INTO user (id, first_name, second_name, middle_name, position, phone, doc_name, doc_number, doc_date, citizen_ship_name, citizen_ship_code, is_identified, version)
-        VALUES (sequence_autoincrement.nextval, 'Vlad', 'Igorevich', 'Zhelezov', 'Meneger',  '89549613229', 'Status', '1', '2014-3-24', null, null, 1, 0);
-
-INSERT INTO user (id, first_name, second_name, middle_name, position ,phone, doc_name, doc_number, doc_date, citizen_ship_name, citizen_ship_code, is_identified,  version)
-        VALUES (sequence_autoincrement.nextval, 'Anton', 'Sergeevich', 'Makarev', 'Ingener', '89349212225', 'One', '2', '2015-2-24', null, null, 1, 0);
-
-INSERT INTO user (id, first_name, second_name, middle_name, position, phone, doc_name, doc_number, doc_date, citizen_ship_name, citizen_ship_code, is_identified,  version)
-        VALUES (sequence_autoincrement.nextval, 'Sergey', 'Vladimirovich', 'Bontor', 'Director',  '89999713479', 'Reception', '3', '2013-8-21', null, null, 1, 0);
-
-INSERT INTO user (id, first_name, second_name, middle_name, position ,phone, doc_name, doc_number, doc_date, citizen_ship_name, citizen_ship_code, is_identified,  version)
-        VALUES (sequence_autoincrement.nextval, 'Anton', 'Petrovich', 'Razmil', 'Ingener', '89543270323', 'Firing', '4', '2016-8-14', null, null, 1, 0);
-
-
-
 INSERT INTO organization (id, name, full_name, inn, kpp, address, phone, is_active, version)
         VALUES (1, 'BellInt', 'BellIntegrator', '54325234', '231433', 'ул. Иванова 7', '2233424',1, 0);
 
 INSERT INTO organization (id, name, full_name, inn, kpp, address, phone, is_active, version)
         VALUES (2, 'Google', 'GG', '84143234', '251221', 'ул. Прыгунова 47', '2235124',1, 0);
-
-
 
 
 INSERT INTO office (id, name, address, phone, is_active, org_id, version)
@@ -27,17 +11,8 @@ INSERT INTO office (id, name, address, phone, is_active, org_id, version)
 INSERT INTO office (id, name, address, phone, is_active, org_id, version)
         VALUES (sequence_table.nextval, 'SecondOffice', 'ул. Иванова 7', '2233424', 1, 1, 0);
 
-INSERT INTO office (id, name, address, phone, is_active, version)
-        VALUES (sequence_table.nextval, 'FirstOffice', 'ул. Иванова 8', '2136434', 1, 0);
-
-
-INSERT INTO doc (id, name, code, version)
-VALUES ( 1, 'Паспорт гражданина РФ', '1453', 0);
-
-INSERT INTO doc (id, name, code, version)
-VALUES ( 2, 'Военный билет', '5432', 0);
-
-
+INSERT INTO office (id, name, address, phone, is_active, org_id, version)
+        VALUES (sequence_table.nextval, 'FirstOffice', 'ул. Иванова 8', '2136434', 1, 2, 0);
 
 
 INSERT INTO country (id, name, code, version)
@@ -45,3 +20,39 @@ INSERT INTO country (id, name, code, version)
 
 INSERT INTO country (id, name, code, version)
         VALUES (2, 'Соединённые Шта́ты Аме́рики' , '6745', 0) ;
+
+
+INSERT INTO doc (id, name, doc_number, doc_date, code, country_id, version)
+        VALUES ( 1, 'Паспорт гражданина РФ', '1', '2014-3-24', '1453', 1, 0);
+
+INSERT INTO doc (id, name, doc_number, doc_date, code, country_id,  version)
+        VALUES ( 2, 'Военный билет', '1', '2015-2-24', '5432', 1, 0);
+
+INSERT INTO doc (id, name, doc_number, doc_date, code, country_id,  version)
+        VALUES ( 3, 'Паспорт гражданина РФ', '1', '2013-8-21', '1453', 2, 0);
+
+INSERT INTO doc (id, name, doc_number, doc_date, code, country_id, version)
+        VALUES ( 4, 'Военный билет', '1', '2016-8-14', '5432', 2, 0);
+
+
+INSERT INTO user (id, first_name, second_name, middle_name, position, phone, citizen_ship_name, citizen_ship_code, is_identified, doc_id, version)
+        VALUES (sequence_autoincrement.nextval, 'Vlad', 'Igorevich', 'Zhelezov', 'Meneger',  '89549613229', null, null, 1, 1, 0);
+
+INSERT INTO user (id, first_name, second_name, middle_name, position ,phone, citizen_ship_name, citizen_ship_code, is_identified, doc_id, version)
+        VALUES (sequence_autoincrement.nextval, 'Anton', 'Sergeevich', 'Makarev', 'Ingener', '89349212225',  null, null, 1, 2, 0);
+
+INSERT INTO user (id, first_name, second_name, middle_name, position, phone, citizen_ship_name, citizen_ship_code, is_identified, doc_id, version)
+        VALUES (sequence_autoincrement.nextval, 'Sergey', 'Vladimirovich', 'Bontor', 'Director',  '89999713479',  null, null, 1, 3, 0);
+
+INSERT INTO user (id, first_name, second_name, middle_name, position ,phone, citizen_ship_name, citizen_ship_code, is_identified, doc_id, version)
+        VALUES (sequence_autoincrement.nextval, 'Anton', 'Petrovich', 'Razmil', 'Ingener', '89543270323', null, null, 1, 4, 0);
+
+
+INSERT INTO user_office (office_id, user_id, version)
+        VALUES (1, 1, 0) ;
+INSERT INTO user_office (office_id, user_id, version)
+        VALUES (2, 2, 0) ;
+INSERT INTO user_office (office_id, user_id, version)
+        VALUES (2, 3, 0) ;
+INSERT INTO user_office (office_id, user_id, version)
+        VALUES (1, 2, 0) ;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,15 +15,41 @@ CREATE INDEX organization_index_id ON organization (id);
 CREATE TABLE IF NOT EXISTS office (
     id         BIGINT      NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
     name       VARCHAR(50) NOT NULL COMMENT 'Название офиса',
-    address    VARCHAR(50) NOT NULL COMMENT 'Адресс организации',
+    address    VARCHAR(50) NOT NULL COMMENT 'Адресс офиса',
     phone      VARCHAR(50)          COMMENT 'Телефон офиса',
     is_active  BIT         NOT NULL COMMENT 'Активный или нет',
-    org_id     INTEGER              COMMENT 'Внешний ключ',
+    org_id     INTEGER              COMMENT 'Внешний ключ на организацию',
     version    INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
-    FOREIGN KEY (org_id) REFERENCES organization(id)
+    
+    FOREIGN KEY (org_id) REFERENCES organization(id),
 );
 COMMENT ON TABLE office IS 'Офис';
 CREATE INDEX office_index_id ON office (id);
+
+
+CREATE TABLE IF NOT EXISTS country (
+    id         BIGINT       NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    name       VARCHAR(50)  NOT NULL COMMENT 'Страны',
+    code       VARCHAR(643) NOT NULL COMMENT 'Код страны',
+    version    INTEGER      NOT NULL COMMENT 'Служебное поле hibernate',
+);
+COMMENT ON TABLE country IS 'Страны';
+CREATE INDEX country_index_id ON country (id);
+
+
+CREATE TABLE IF NOT EXISTS doc (
+    id         BIGINT      NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    name       VARCHAR(50) NOT NULL COMMENT 'Название документа',
+    doc_number VARCHAR(50) NOT NULL COMMENT 'Номер документа',
+    doc_date   DATE        NOT NULL COMMENT 'Дата выдачи',
+    code       VARCHAR(21) NOT NULL COMMENT 'Код документа',
+    country_id BIGINT      NOT NULL COMMENT 'Внешний ключ на страну',
+    version    INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+
+    FOREIGN KEY (country_id) REFERENCES country(id),
+);
+COMMENT ON TABLE doc IS 'Справочники';
+CREATE INDEX doc_index_id ON doc (id);
 
 CREATE TABLE IF NOT EXISTS user (
     id                    BIGINT      NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
@@ -32,51 +58,25 @@ CREATE TABLE IF NOT EXISTS user (
     middle_name           VARCHAR(50) NOT NULL COMMENT 'Фамилия',
     position              VARCHAR(50) NOT NULL COMMENT 'Должность',
     phone                 VARCHAR(50) NOT NULL COMMENT 'Телефон сотрудника',
-    doc_name              VARCHAR(50) NOT NULL COMMENT 'Название документа',
-    doc_number            VARCHAR(50) NOT NULL COMMENT 'Номер документа',
-    doc_date              DATE        NOT NULL COMMENT 'Дата выдачи',
     citizen_ship_name     VARCHAR(50)          COMMENT 'Гражданское имя',
     citizen_ship_code     VARCHAR(50)          COMMENT 'Гражданский код',
     is_identified         BIT         NOT NULL COMMENT 'Активный или нет',
-    country_id            INTEGER              COMMENT 'Внешний ключ страны',
+    doc_id                BIGINT      NOT NULL COMMENT 'Внешний ключ на документ',
     version               INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
 
+    FOREIGN KEY (doc_id) REFERENCES doc(id),
 
-    FOREIGN KEY (country_id) REFERENCES Country(id),
 );
 COMMENT ON TABLE user IS 'Сотрудник';
 CREATE INDEX user_index_id ON user (id);
 
 
-CREATE TABLE IF NOT EXISTS doc (
-    id         BIGINT      NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    name       VARCHAR(50) NOT NULL COMMENT 'Название документа',
-    code       VARCHAR(21) NOT NULL COMMENT 'Код',
-    version    INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
-);
-COMMENT ON TABLE doc IS 'Справочники';
-CREATE INDEX doc_index_id ON doc (id);
-
-
-CREATE TABLE IF NOT EXISTS country (
-    id         BIGINT       NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    name       VARCHAR(50)  NOT NULL COMMENT 'Страны',
-    code       VARCHAR(643) NOT NULL COMMENT 'Код',
-    version    INTEGER      NOT NULL COMMENT 'Служебное поле hibernate',
-);
-COMMENT ON TABLE country IS 'Страны';
-CREATE INDEX country_index_id ON country (id);
-
-
-CREATE TABLE IF NOT EXISTS proxy_table (
-    id         BIGINT       NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    doc_id     BIGINT       NOT NULL COMMENT 'Внешний ключ документа',
-    country_id BIGINT       NOT NULL COMMENT 'Внешний ключ страны',
+CREATE TABLE IF NOT EXISTS user_office (
     office_id  BIGINT       NOT NULL COMMENT 'Внешний ключ офиса',
     user_id    BIGINT       NOT NULL COMMENT 'Внешний ключ пользователя',
     version    INTEGER      NOT NULL COMMENT 'Служебное поле hibernate',
 );
-COMMENT ON TABLE proxy_table IS 'Таблица для связи ManyToMany';
+COMMENT ON TABLE user_office IS 'Таблица для связи ManyToMany';
 
 
 CREATE SEQUENCE sequence_table
